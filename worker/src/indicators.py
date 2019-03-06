ed25d5c2004c0c9bfb7d705db202827194002fe3
marcusmello <mandealista@gmail.com>
2019-03-06 12:06:35 -0300

    pandazord_charlie v0.1.0 | Started indicators module

---
+++ worker/src/indicators.py
@@ -0,0 +1,19 @@
+#indicators.py
+
+#!/usr/bin/env python3
+# coding: utf-8
+
+class cross_ma:
+    #Init
+    def __init__(self,input_data,short_ma,long_ma):
+        self.input_data=input_data
+        self.short_ma=short_ma
+        self.long_ma=long_ma
+
+        #return mute?
+    
+    def side(self):
+        
+        print(self.short_ma+' By module 1, class 1 and method 1')
+
+         #return mute?
