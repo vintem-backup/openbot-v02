77274a323a4e540618c8a7cdec8463fe705c4539
marcusmello <mandealista@gmail.com>
2019-03-31 20:07:25 -0300

    OpenBot v0.1.2 | Update run.sh and dev folder

--- run.sh
+++ run.sh
@@ -3,25 +3,30 @@
 : '
 Usage: ./run.sh <op>
 <op>:
-[0] - Default | Run the cluster on a AWS, from remote container`s images.
-[1] - Dev 1   | Run the cluster on local machine, from locally built container`s images.
-[2] - Dev 2   | Build the containers` images and push them to specific docker registry.
-[3] - Dev 3   | Run the cluster on local machine, from remote container`s images.
-[4] - Dev 4   | Up the dev database, for python tests
+[0] - Production | Run the production cluster on a AWS, from remote container`s images.
+[1] - Staging 1  | Run the production cluster (build and run) on local machine.
+[2] - Delivery   | Build the containers` images and push them to specific docker registry.
+[3] - Staging 2  | Run the production cluster on local machine, from remote container`s images.
+[4] - Dev 1      | Up the dev database (build and run), for python tests on get_data and/or worker.
+[5] - Dev 2      | Up the dev database and get_data (build and run), for python tests on worker.
+[6] - Dev 3      | Run the dev cluster on local machine (build and run).
 '
-
-export env_file="dev.env"  
 
 if [ $1 = 0 ]; then
     export env_file="/efs/.env"
-    if [ -f .env ]; then
-        set -a
-        . ./efs/.env
-        set +a
+else
+    export env_file="dev.env"
+fi
 
-        #update the vm.max_map_count system limit: 
+if [ -f $env_file ]; then
+    set -a
+    . ./$env_file
+    set +a
+
+    if [ $1 = 0 ]; then
+        #update the vm.max_map_count system limit:
         sudo sysctl -w vm.max_map_count=262144
-    
+        
         #Recupera o ID desta instância:
         instanceid=`/usr/bin/curl -s http://169.254.169.254/latest/meta-data/instance-id`
         #OBS.: É preciso liberar a porta 80 para este CIDR (169.254.169.254/32)
@@ -32,98 +37,66 @@ if [ $1 = 0 ]; then
         #Loga no host das imagens docker
         docker login $docker_image_host -u $registry_login -p $registry_password
 
+        #Para o cluster e o renicia
         docker-compose -f ./docker-compose-remote.yml down
         docker-compose -f ./docker-compose-remote.yml -p $operation_name up -d 
 
         #rm -f .env #Uncomment for safe
 
-    else
-        echo 'Arquivo .env não encontrado.'
-
-    fi
-
-elif [ $1 = 1 ]; then
-    if [ -f dev.env ]; then
-        set -a
-        . ./dev.env
-        set +a
-
-        #update the vm.max_map_count system limit: 
+    elif [ $1 = 1 ]; then
         sudo sysctl -w vm.max_map_count=262144
-    
+        
         docker-compose -f ./docker-compose-local.yml down
         docker-compose -f ./docker-compose-local.yml -p $operation_name build --no-cache
         docker-compose -f ./docker-compose-local.yml -p $operation_name up #-d
-    
-    else
-        echo 'Arquivo dev.env não encontrado.'
-
-    fi
-
-elif [ $1 = 2 ]; then
-    if [ -f dev.env ]; then
-        set -a
-        . ./dev.env
-        set +a
         
+    elif [ $1 = 2 ]; then
         #Imagem do container get_data
         docker build --no-cache -t $docker_image_host/$image_path/get_data -f get_data/Dockerfile .
 
         #Imagem do worker
         docker build --no-cache -t $docker_image_host/$image_path/worker -f worker/Dockerfile .
-    
-        #Loga no host das imagens docker
+        
         docker login $docker_image_host -u $registry_login -p $registry_password
 
         #Push das imagens
         docker push $docker_image_host/$image_path/get_data
         docker push $docker_image_host/$image_path/worker
 
-    else
-        echo 'Arquivo dev.env não encontrado.'
-    fi
-
-elif [ $1 = 3 ]; then
-    if [ -f dev.env ]; then
-        set -a
-        . ./dev.env
-        set +a
-        
-        #update the vm.max_map_count system limit: 
+    elif [ $1 = 3 ]; then
         sudo sysctl -w vm.max_map_count=262144
 
-        #Loga no host das imagens docker
         docker login $docker_image_host -u $registry_login -p $registry_password
 
-        #Sobe o cluster
         docker-compose -f ./docker-compose-remote.yml down
         docker-compose -f ./docker-compose-remote.yml -p $operation_name up -d
 
-    else
-        echo 'Arquivo dev.env não encontrado.'
-    fi
-
-elif [ $1 = 4 ]; then
-    if [ -f dev.env ]; then
-        set -a
-        . ./dev.env
-        set +a
-        
-        #update the vm.max_map_count system limit: 
+    elif [ $1 = 4 ]; then
         sudo sysctl -w vm.max_map_count=262144
+        
+        docker-compose -f ./dev/src/compose1.yml down
+        docker-compose -f ./dev/src/compose1.yml -p $operation_name build --no-cache
+        docker-compose -f ./dev/src/compose1.yml -p $operation_name up #-d
+        
+    elif [ $1 = 5 ]; then
+        sudo sysctl -w vm.max_map_count=262144
+        
+        docker-compose -f ./dev/src/compose2.yml down
+        docker-compose -f ./dev/src/compose2.yml -p $operation_name build --no-cache
+        docker-compose -f ./dev/src/compose2.yml -p $operation_name up #-d
 
-        #Loga no host das imagens docker
-        docker login $docker_image_host -u $registry_login -p $registry_password
-
-        #Sobe o cluster
-        docker-compose -f ./docker-compose-remote.yml down
-        docker-compose -f ./docker-compose-remote.yml -p $operation_name up -d
+    elif [ $1 = 6 ]; then
+        sudo sysctl -w vm.max_map_count=262144
+        
+        docker-compose -f ./dev/src/compose3.yml down
+        docker-compose -f ./dev/src/compose3.yml -p $operation_name build --no-cache
+        docker-compose -f ./dev/src/compose3.yml -p $operation_name up #-d
 
     else
-        echo 'Arquivo dev.env não encontrado.'
+        echo "Opção inválida para o setup do cluster"
+
     fi
 
 else
-    echo "Opção inválida para o setup do cluster"
-
+    echo 'Arquivo .env não encontrado.'
 fi
